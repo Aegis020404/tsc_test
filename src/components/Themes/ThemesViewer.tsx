d4b28c703778e6343f233a74ff6a4ecdb7484f8f
lomysbro <Aegis020404@gmail.com>
2023-05-03 18:24:37 +0300

    add ThemesViewer

--- src/components/Themes/ThemesViewer.tsx
+++ src/components/Themes/ThemesViewer.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import cl from '../../styles/Themes/ThemesViewer.module.css';
+import backedImg from '../../assets/imgs/backed.png';
+import unwrapImg from '../../assets/imgs/unwrap.png';
 
 const ThemesViewer = () => {
     return (
         <div className={cl.ThemesViewer}>
-
+            <div className={cl.management}>
+                <div className={cl.btns}>
+                    <button className={cl.btn}>× Очистить</button>
+                    <button className={cl.btn}><img src={backedImg} width={10} height={12} alt="backet img"/> Выбрать</button>
+                </div>
+                <div className={cl.unwrapper}>
+                    <button className={cl.btn +' '+ cl.unwrap}><img src={unwrapImg} width={10} height={12} alt="unwrap img"/> Развернуть</button>
+                </div>
+            </div>
         </div>
     );
 };
